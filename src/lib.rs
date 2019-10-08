a3f553ad8dfda974181cd5263e5424855ee4674a
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-10-08 13:49:01 +0200

    Extract message to module

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ use std::io::{Error, Write, Read};
 use buf_redux::Buffer;
 use std::thread;
 use std::convert::TryInto;
+use message::Message::{LoginRequest};
+use std::borrow::Borrow;
+use std::ops::Deref;
+
+mod message;
 
 pub struct Slsk {
     server: &'static str,
@@ -49,40 +54,9 @@ impl Slsk {
                     }
                 });
 
-                let message_type: u32 = 1;
-                let username = self.username.as_bytes();
-                let password = self.password.as_bytes();
-                let unknown_field: u32 = 160;
-                let another_unknown_field: u32 = 0x11;
-                let cred = format!("{}{}", self.username, self.password);
-                println!("credentials: {}", cred);
-                let computed = md5::compute(format!("{}{}", self.username, self.password));
-                let computed_string = format!("{:x}", computed);
-                println!("hex credentials: {}", computed_string.as_str());
+                let loginRequest = LoginRequest(self.username, self.password);
 
-                let mut message = Buffer::new();
-                message.push_bytes(&message_type.to_le_bytes());
-                let username_len: u32 = username.len() as u32;
-                message.push_bytes(&username_len.to_le_bytes());
-                message.push_bytes(username);
-                let password_len: u32 = password.len() as u32;
-                message.push_bytes(&password_len.to_le_bytes());
-                message.push_bytes(password);
-                message.push_bytes(&unknown_field.to_le_bytes());
-                let hex_credentials_len: u32 = computed_string.as_str().as_bytes().len() as u32;
-                message.push_bytes(&hex_credentials_len.to_le_bytes());
-                message.push_bytes(&computed_string.as_str().as_bytes());
-                message.push_bytes(&another_unknown_field.to_le_bytes());
-
-                let mut complete = Buffer::new();
-                let message_len: u32 = message.len() as u32;
-                complete.push_bytes(&message_len.to_le_bytes());
-                complete.push_bytes(message.buf());
-
-                println!("{:?}", complete);
-                println!("{:?}", complete.buf());
-
-                match server.write(complete.buf()) {
+                match server.write(loginRequest.as_buffer().buf()) {
                     Ok(count) => println!("Writed {} bytes to server", count),
                     Err(e) => panic!(e)
                 }
